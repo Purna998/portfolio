176d8a9d1a8334e54ff3a90ca3d99a693baba675
Purna Acharya <152753500+Purna998@users.noreply.github.com>
2025-08-12 14:05:09 +0000

    next

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,11 +1,15 @@
+// main.jsx (or index.jsx)
 import { StrictMode } from 'react'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from "./App"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* Keep App and SpeedInsights as siblings */}
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
